f9529a3bc0c7b926536945cdfbcbbc15cff354a0
GouTao <457754603@qq.com>
2016-02-22 10:31:25 +0800

    第一版

---
+++ js/app.js
@@ -0,0 +1,153 @@
+var ifMj=false,ifName=false,ifTel=false,ifGroup=false;
+var regCode="";
+var infoArr=[],infoLength=0,tempShow=0,changeTime;
+
+$(window).resize(function(){
+	$(".forFooter").css('height',$(".footer").height());
+})
+$(document).ready(function(){
+	var getInfo=new Object();
+	getInfo.command="getInfo";
+	
+	$.ajax({
+		type:"post",
+		url:"http://119.29.75.226:11911",
+		async:true,
+		data:{"data":JSON.stringify(getInfo)},
+		dataType:"json",
+		success:function(res){
+			if(res.result=="success"){
+				infoArr=res.data;
+				createInfoBoard();
+			}
+		},
+		error:function(){
+			console.log("err");
+		}
+	});
+	
+	
+	$(".forFooter").css('height',$(".footer").height());
+	
+	$("#enterBtn").bind("click",function(){
+		open("show.html","_self");
+	});
+	$("#regBtn").bind("click",function(){
+		$("#mj").val()==''?ifMj=false:ifMj=true;
+		$("#name").val()==''?ifName=false:ifName=true;
+		ifTel=regTel($("#tel").val());
+		$("#group").val()==''?ifGroup=false:ifGroup=true;
+		
+		if(ifMj&&ifName&&ifGroup&&ifTel){
+			regCode="";
+			for(var i=0;i<6;i++){
+				regCode+=Math.floor((Math.random()*10)).toString();
+			}
+			$("#codePre").html(regCode);
+			$("#myModal2").modal();
+		}
+		else{
+			alert("请确保您已经按要求完整填写所属民建分支机构名称、姓名、联系方式、企业或团体名称四项内容");
+		}
+	})
+	$("#subBtn").unbind("click").bind("click",function(){
+		if($("#code").val()==regCode){
+			var sendObj=new Object;
+			sendObj.command="sendEmail";
+			sendObj.branchName=$("#mj").val();
+			sendObj.name=$("#name").val();
+			sendObj.phoneNum=$("#tel").val();
+			sendObj.organization=$("#group").val();
+			var sendStr=JSON.stringify(sendObj);
+			$.ajax({
+				type:"post",
+				url:"http://115.159.39.71:6007",
+				async:true,
+				data:{"data":sendStr},
+				dataType:"json",
+				success:function(res){
+					if(res.result=="success"){
+						alert("发送申请信息成功。");
+						$("#name").val("");
+						$("#tel").val("");
+						$("#group").val("");
+						$("#myModal2").modal("hide");
+					}
+					else{
+						aler("发送失败，请尝试重新发送信息。");
+						$("#myModal2").modal("hide");
+					}
+					
+				},
+				error:function(){
+					aler("链接失败，请尝试重新发送信息。");
+					$("#myModal2").modal("hide");
+				}
+			});
+		}
+		else{
+			alert("验证码错误！")
+		}
+	})
+})
+
+function createInfoBoard(){
+	for(var i=0;i<infoArr.length;i++){
+		var $content=$(
+			"<div>"+
+				"<p style='text-indent: 0;font-size: 1.1em;'>发布者:<small>"+infoArr[i].user+"</small></p>"+
+				"<p style='text-indent: 0;font-size: 1.1em;'>发布时间:<small>"+infoArr[i].time+"</small></p>"+
+				"<p style='text-indent: 0;font-size: 1.1em; '>内容:<small style='word-break:break-all'>"+infoArr[i].content+"</small></p>"+
+			"</div>"	
+		)
+		if(i!=0){
+			$content.css('display',"none");
+		}
+		$("#infoContent").append($content);
+	}
+	
+	infoLength=$("#infoContent").children().length;
+	$("#tempNum").html(tempShow+1);
+	$("#totalNum").html(infoLength);
+	
+	infoChange();
+	
+	$("#lastBtn").on('click',function(){
+		$($("#infoContent").children()[tempShow]).css("display","none");
+		tempShow==0?tempShow=infoLength-1:tempShow=tempShow-1;
+		$($("#infoContent").children()[tempShow]).css("display","block");
+		$($("#infoContent").children()[tempShow]).removeClass("animated fadeIn");
+		$($("#infoContent").children()[tempShow]).addClass("animated fadeIn");
+		$("#tempNum").html(tempShow+1);
+		clearInterval(changeTime);
+		infoChange();
+	});
+	$("#nextBtn").on('click',function(){
+		$($("#infoContent").children()[tempShow]).css("display","none");
+		tempShow==infoLength-1?tempShow=0:tempShow=tempShow+1;
+		$($("#infoContent").children()[tempShow]).css("display","block");
+		$($("#infoContent").children()[tempShow]).removeClass("animated fadeIn");
+		$($("#infoContent").children()[tempShow]).addClass("animated fadeIn");
+		$("#tempNum").html(tempShow+1);
+		clearInterval(changeTime);
+		infoChange();
+	})
+	
+	
+}
+
+function infoChange(){
+	changeTime=setInterval(function(){
+		$($("#infoContent").children()[tempShow]).css("display","none");
+		tempShow==infoLength-1?tempShow=0:tempShow=tempShow+1;
+		$($("#infoContent").children()[tempShow]).css("display","block");
+		$($("#infoContent").children()[tempShow]).removeClass("animated fadeIn");
+		$($("#infoContent").children()[tempShow]).addClass("animated fadeIn");
+		$("#tempNum").html(tempShow+1);
+	},5000);
+}
+
+function regTel(tel){
+	var rex=/^1\d{10}$/;
+	return rex.test(tel);
+}
---
+++ js/show.js
@@ -0,0 +1,243 @@
+			var itemContent = $(".show_item");//item容器
+			var items = [];
+			var noIcon = $("#nothing");
+			var windowHeight;
+			$(function(){
+				checkSys();
+//				che
+				getData();
+				//切换搜索
+				$("#show_search_change_btn").on("click",function(e){
+					if($("#show_search_change").html() == "名称查询")
+					{
+						$("#show_search_change").html("支部查询");
+					}
+					else
+					{
+						$("#show_search_change").html("名称查询");
+					}
+				})
+				$("#show_search_change").on("click",function(e){
+
+					if($("#show_search_change").html() == "名称查询")
+					{
+						$("#show_search_change").html("支部查询");
+					}
+					else
+					{
+						$("#show_search_change").html("名称查询");
+					}
+				})
+				//搜索
+				$("#show_search_btn").on("click",function(e){
+					
+					var txt = $(".show_search > input").val()
+					var command = $(".show_search_change").html() == "名称查询"?"findByName":"findByArea";
+					
+					
+					if(txt!=""){
+						var sendData=new Object();
+						sendData.command=command;//getList
+						sendData.findKey = txt;
+						var sendMsg=JSON.stringify(sendData);
+						$.ajax({
+							url:"http://119.29.75.226:11911",
+							async:true,
+							type: "post",
+							dataType: "json",
+							data:{"data":sendMsg},
+							timeout:500,
+							success: function(data, textStatus)
+							{
+								itemContent.empty();
+								
+								if(data.data.length == 0){
+								
+									noIcon.css("display","inline-block");
+									itemContent.append(noIcon);
+									return;
+								}
+								$.each(data.data, function() {
+									var name = this.name;//名称
+									var pcUrl = this.cover;//图片地址
+									var showNum = this.showNum;//热读
+									var address = this.code;//链接地址
+									var time = this.createdTime;//创建时间
+									var type = this.type;//所属类型
+									
+									var item = creatIconItem(name,pcUrl,address,showNum,time,type);
+									console.log(this);
+									itemContent.append(item);
+								});
+							},
+							error:function(){
+
+							}
+						});
+					}
+					
+				})
+				
+				//返回上级界面
+				$("#returnToMain").on("click",function(e){
+					open("index.html","_self");
+				})
+				//下拉菜单样式及功能
+				$(".show_nav_btn").on("mouseover",function(e){
+					$(this).find(".show_nav_2").show();
+				})
+				$(".show_nav_btn").on("mouseout",function(e){
+					$(this).find(".show_nav_2").hide();
+				})
+				$(".show_nav_2_btn").on("mousedown",function(e){
+					$(this).parent().hide();
+					var command = $(this).html();
+					if(command.charAt(0) == "最")//时间热度
+					{
+						itemContent.empty();
+						switch(command)
+						{
+							case "最少浏览":
+									items.sort(function(a,b){return Number(a.attr("showNum"))>Number(b.attr("showNum"))?1:-1});
+									for(var i = 0;i<items.length;i++){
+										console.log(items[i].attr("showNum"));
+										itemContent.append(items[i]);
+									}
+								break;
+							case "最多浏览":
+									items.sort(function(a,b){return Number(a.attr("showNum"))<Number(b.attr("showNum"))?1:-1});
+									for(var i = 0;i<items.length;i++){
+										console.log(items[i].attr("showNum"));
+										itemContent.append(items[i]);
+									}
+								break;
+							case "最新发布":
+									items.sort(function(a,b){return Number(a.attr("time"))<Number(b.attr("time"))?1:-1});
+									for(var i = 0;i<items.length;i++){
+										console.log(items[i].attr("time"));
+										itemContent.append(items[i]);
+									}
+								break;
+							case "最早发布":
+									items.sort(function(a,b){return Number(a.attr("time"))>Number(b.attr("time"))?1:-1});
+									for(var i = 0;i<items.length;i++){
+										console.log(items[i].attr("time"));
+										itemContent.append(items[i]);
+									}
+								break;
+						}
+					}
+					else//所属类型
+					{
+
+						itemContent.empty();
+						for(var i = 0;i<items.length;i++)
+						{
+							if(items[i].attr("type") == command)
+							{
+								itemContent.append(items[i]);
+								return;
+							}
+						}
+						noIcon.css("display","inline-block");
+						itemContent.append(noIcon);
+					}
+				})
+			})
+			$(window).resize(onResize);
+			function onResize(){
+//				windowHeight = $(window).;
+				console.log(windowHeight);
+			}
+			function checkSys() { 
+				var sUserAgent = navigator.userAgent.toLowerCase();
+	            var bIsIpad = sUserAgent.match(/ipad/i) == "ipad";
+	            var bIsIphoneOs = sUserAgent.match(/iphone os/i) == "iphone os";
+	            var bIsMidp = sUserAgent.match(/midp/i) == "midp";
+	            var bIsUc7 = sUserAgent.match(/rv:1.2.3.4/i) == "rv:1.2.3.4";
+	            var bIsUc = sUserAgent.match(/ucweb/i) == "ucweb";
+	            var bIsAndroid = sUserAgent.match(/android/i) == "android";
+	            var bIsCE = sUserAgent.match(/windows ce/i) == "windows ce";
+	            var bIsWM = sUserAgent.match(/windows mobile/i) == "windows mobile";
+	            console.log("您的浏览设备为：");
+	            if (bIsIpad || bIsIphoneOs || bIsMidp || bIsUc7 || bIsUc || bIsAndroid || bIsCE || bIsWM) {
+	                 return "phone";
+	            } else {
+	                 return "pc";
+	            }
+			} 
+
+			
+			/**
+			 * 
+			 * @param 名称
+			 * @param 图片地址
+			 * @param 链接地址
+			 * @param 热度
+			 * @param 创建时间
+			 * @paran 类型
+			 */
+			function creatIconItem(name,pc,url,showNum,time,type){
+				var $item=$(
+					"<div  class='item'  showNum='"+showNum+"' time='"+time+"' type='"+type+"'>"+
+						"<img style='width: 100%;' src='"+pc+"'/><br /><br />"+
+						"<p style='color: #00A1CB;'>"+name+"</p>"+
+						"<p><b>行业类型：</b>"+type+"</p>"+
+						"<p><b>浏览热度：</b>"+showNum+"</p>"+
+						"<p><b>发布时间：</b>"+new Date(parseInt(time)).toLocaleString().replace(/年|月/g, "-").replace(/日/g, " ") +"</p>"+
+						"<p class='url' style='display: none;'>"+url+"</p>"+
+					"</div>"
+				)
+//				$item.css({
+//					"width":"300px",
+//					"background-color":"white",
+//					"padding":"5px",
+//					"margin":"40px",
+//					 "display":"inline-block",
+//					 "box-shadow":"2px 2px 10px #cccccc"
+//				})
+				$item.children('p').each(function(){
+					$(this).css({
+						"padding":"0 10px",
+					})
+				})
+				$item.on('mousedown ',function(e){
+					open($(e.currentTarget).children('.url').html(),"_blank");
+				})
+				return $item;
+			}
+
+			//初始化数据
+			function getData(){
+				var sendData=new Object();
+				sendData.command="getList";//getList
+				var sendMsg=JSON.stringify(sendData);
+				$.ajax({
+					url:"http://119.29.75.226:11911",
+					async:true,
+					type: "post",
+					dataType: "json",
+					data:{"data":sendMsg},
+					timeout:500,
+					success: function(data, textStatus)
+					{
+						$.each(data.data, function() {
+							var name = this.name;//名称
+							var pcUrl = this.cover;//图片地址
+							var showNum = this.showNum;//热读
+							var address = this.code;//链接地址
+							var time = this.createdTime;//创建时间
+							var type = this.type;//所属类型
+							
+							var item = creatIconItem(name,pcUrl,address,showNum,time,type);
+							items.push(item);
+
+							itemContent.append(item);
+						});
+					},
+					error:function(){
+//						console.log("XX");
+					}
+				});
+			}
+			
